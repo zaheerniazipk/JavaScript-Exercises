794d98f63cd0c08002d2846a4cc56dd1ddc9a1f6
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-25 22:19:46 +0500

    JS and DOM manipulation

---
+++ 3.shoppingList-DOM-manipulation/script.js
@@ -0,0 +1,35 @@
+var title = document.querySelector("h1");
+var input = document.getElementById("userinput");
+var button = document.getElementById("enter");
+var ul = document.querySelector("ul");
+
+
+// Code Refactoring
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+// Add CSS Class
+// title.classList.add("coolTitle");
